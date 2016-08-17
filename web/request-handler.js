e67ac52f855213029b78e5450258bbc2d6e26a4f
corwinc <corwin.crownover@gmail.com>
2016-08-17 16:58:27 -0700

    Pass first test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,55 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
+var sendResponse = function (res, data, statusCode) {
+  statusCode = statusCode || 200;
+  res.writeHead(statusCode, httpHelpers.headers);
+  res.end(data);
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log(__dirname);
+  if (req.method === 'GET') {
+    //console.log(req.url);
+    if (req.url === '/') {
+      fs.readFile(path.join(__dirname, '/public/index.html'), 'utf8', function(err, data) {
+        // if (data) {
+        if (err) { throw err; }
+          console.log('data: ', data);
+          httpHelpers.headers['Content-Type'] = 'text/html';
+          sendResponse(res, data);
+        //}
+
+        // if (err) {
+        //   console.log('threw error with get request to /');
+        // }
+
+      });
+    } else if (req.url === '/styles.css') {
+      console.log('sent css');
+      fs.readFile(path.join(__dirname, '/public/styles.css'), 'utf8', function(err, data) {
+        // if (err) {
+        //   console.log('threw error with get request to /styles.css');
+        // }
+        httpHelpers.headers['Content-Type'] = 'text/css';
+        sendResponse(res, data);
+      });
+    }
+  } else if (req.method === 'POST') {
+    console.log('received POST request');
+    // var data = '';
+    // req.on('data', function (chunk) {
+    //   data += chunk;
+    // });
+    // httpHelpers.headers['Content-Type'] = 'application/json';
+    // sendResponse(res, data);
+
+  } else if (req.method === 'OPTIONS') {
+
+  }
+
+  // res.end(archive.paths.list);
 };
